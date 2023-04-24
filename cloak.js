94b213945771eea33a26803de7b94e59e5d9b7bf
Derpman#0858 <91960533+DerpWerp@users.noreply.github.com>
2023-04-24 17:38:14 -0400

    deleted some useless scripts

--- cloak.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    let url = (window.location.href, "/app.html");
-    var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999999,height=99999")
-    w.document.write('<script>document.title = "Wikipedia";document.tabIcon = "https://en.wikipedia.org/favicon.ico"</script>' +
-'<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="'+url+'"></iframe>');
-})();
