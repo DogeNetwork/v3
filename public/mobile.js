afe8ddc7b4773b578d4247bf4dcc96eebfe2f7f6
Derpman#0858 <91960533+DerpmanDev@users.noreply.github.com>
2023-05-09 20:57:45 -0400

    test

--- public/mobile.js
+++ public/mobile.js
@@ -3,7 +3,7 @@ let regexp = /android|iphone|kindle|ipad/i;
 let isMobileDevice = regexp.test(details);
 if (isMobileDevice) {
     alert("Doge Unblocker has detected that you are accessing the website on mobile. Please use a computer instead as this website was not designed for mobile devices.");
-  document.write('<iframe src="mobile.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>')
+  document.write('<iframe src="mobile.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>');
 } else {
-    console.log("You are using Desktop");
+    document.write('<iframe src="app.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%; overflow: hidden;"></iframe>');
 }
