19c399dfb16e36906ea9a9b72ffee0422ee95c8a
Derpman <immapotato8110@gmail.com>
2023-07-06 14:35:02 -0400

    fix loading page instead of 404

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', async event => {
         let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-        document.write('<iframe src="404.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%;"></iframe>');
+        document.write('<iframe src="loading.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%;"></iframe>');
         window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
     });
 });
