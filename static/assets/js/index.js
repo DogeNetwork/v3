9077d37d198bcefc2f66efc0d6395e027848a3ee
Derpman <immapotato8110@gmail.com>
2023-07-07 22:33:30 +0000

    Update index.js

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -9,9 +9,9 @@ form.addEventListener('submit', async event => {
         let url = input.value.trim();
         if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
         else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-        document.write('<iframe src="loading.html" style="position: fixed; inset: 0px; outline: none; border: none; height: 100%; width: 100%;"></iframe>');
+        sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
         location.href = 'go.html';
-        // window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
+        // window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url) balls
     });
 });
 
