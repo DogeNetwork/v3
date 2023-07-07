a6e41cc436c8ef9d8692040ca5c19a9e882d5c11
Derpman <immapotato8110@gmail.com>
2023-07-07 23:34:15 +0000

    i suck at backend code

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -4,7 +4,7 @@ const input = document.querySelector("input");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   window.navigator.serviceWorker
-    .register("./sw.js", {
+    .register("/uv/sw.js", {
       scope: __uv$config.prefix,
     })
     .then(() => {
