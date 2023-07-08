8dcae21c0f8263f13a59a8b4a40489b0bc517da3
Derpman <immapotato8110@gmail.com>
2023-07-08 00:46:01 +0000

    add /x pointing to go

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const ultraviolet = new Easyviolet();
 ultraviolet.httpServer(server);
 server.on('request', (req, res) => { if (!ultraviolet.requiresRoute(req)) app(req, res) });
 app.use(express.static('static', { extensions: ['html'] }));
+app.get('/x', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/go.html'));
+});
 app.use((req, res) => {
   res.statusCode = 404;
   res.sendFile(path.join(process.cwd(), './static/404.html'))
