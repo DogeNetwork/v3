29ce6c261d2cb443ada0ae449919dfc0eba7df60
Derpman <immapotato8110@gmail.com>
2023-07-09 01:42:12 +0000

    visit last site

--- static/assets/js/rightclick.js
+++ static/assets/js/rightclick.js
@@ -68,6 +68,10 @@ function openWindow() {
   w.document.write('<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
 }
 
+function visitLastSite() {
+location.href = '/x';
+}
+
 // Add event listeners to show/hide the context menu
 document.addEventListener("contextmenu", showContextMenu);
 document.addEventListener("click", hideContextMenu);
