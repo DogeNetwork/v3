2fbfff83aafb8709e72d635cc6f15ea86a62b4ea
Derpman <immapotato8110@gmail.com>
2023-07-13 17:12:25 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const ultraviolet = new Easyviolet();
 ultraviolet.httpServer(server);
 server.on('request', (req, res) => { if (!ultraviolet.requiresRoute(req)) app(req, res) });
 app.use(express.static('static', { extensions: ['html'] }));
+app.get('/app', (req, res) => {
+  res.sendFile(path.join(process.cwd(), './static/unlocked.html'));
+});
 app.get('/x', (req, res) => {
   res.sendFile(path.join(process.cwd(), './static/loader.html'));
 });
