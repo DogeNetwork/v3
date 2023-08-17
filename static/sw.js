d40985b35e08c9a04426ac987f7387b03c1bb558
Derpman <immapotato8110@gmail.com>
2023-08-17 19:37:41 -0400

    im gay

--- static/sw.js
+++ static/sw.js
@@ -5,9 +5,9 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
-importScripts('uv.bundle.js');
-importScripts('uv.config.js');
-importScripts(__uv$config.sw || 'uv.sw.js');
+importScripts('/uv/uv.bundle.js');
+importScripts('/uv/uv.config.js');
+importScripts(__uv$config.sw || '/uv/uv.sw.js');
 
 const sw = new UVServiceWorker();
 
