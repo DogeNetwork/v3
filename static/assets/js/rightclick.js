2230a06773f0f97caad0797d3ee91c9163992e93
Derpman <immapotato8110@gmail.com>
2023-08-27 20:14:15 -0400

    updates

--- static/assets/js/rightclick.js
+++ static/assets/js/rightclick.js
@@ -69,8 +69,9 @@ function tabCloak() {
   
   function openWindow() {
     let url = window.location.href;
-    var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999999,height=99999");
+    var w = window.open("about:blank", "_blank");
     w.document.write('<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
+    window.location.href = 'https://classroom.google.com';
   }
   
   function visitLastSite() {
